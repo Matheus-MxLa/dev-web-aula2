87b13d05db5655c2944e1232ec082eec9aa2b5ac
Matheus-MxLa <matheuslacerdamaximiano@gmail.com>
2026-02-24 09:34:18 -0300

    Aula 2

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+    /* Aula 2 v-model
+    import { ref } from 'vue'
+    const mostrar = ref(false)
+    const texto = ref('')
+    */
+    import { ref } from 'vue'
+    const contador = ref(0)
+
+    function incrementar() {
+            contador.value++
+    }
+    function decrementar() {
+        contador.value--
+    }
+</script>
+
+<template>
+    <button :disabled="contador <= 0" @click="decrementar">Decrementar</button>
+    <button @click="incrementar">incrementar</button>   
+    <p>{{ contador }}</p>
+
+   <!-- Aula 2 v-model
+    <div>
+        <input type="text" v-model="texto">
+        <label>{{ texto }}</label>
+        <button @click="mostrar = !mostrar">Mostrar/Ocultar</button>
+        <div v-if="mostrar">
+            <p>Conteúdo exibido.</p>
+        </div>
+        <div v-else>
+            <p>Conteúdo ocultado.</p>
+        </div>
+    </div>
+    -->
+
+</template>
+
+<style scoped>
+
+</style>
